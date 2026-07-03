acc91fd8b3067e4f8f9e25f3685176e66c3e20f3
Harshusinghal2802 <harshsinghal2820@gmail.com>
2026-07-03 13:11:06 +0530

    Initial commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,27 +3,41 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: '/api',
   withCredentials: true,
-  headers: { 'Content-Type': 'application/json' }
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // Request interceptor - attach token
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('dl_token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-}, err => Promise.reject(err));
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('dl_token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
 
 // Response interceptor - handle 401
 api.interceptors.response.use(
-  res => res,
-  err => {
+  (res) => res,
+  (err) => {
     if (err.response?.status === 401) {
       localStorage.removeItem('dl_token');
       localStorage.removeItem('dl_user');
-      if (!window.location.pathname.includes('/login') && !window.location.pathname === '/') {
+
+      if (
+        !window.location.pathname.includes('/login') &&
+        window.location.pathname !== '/'
+      ) {
         window.location.href = '/login';
       }
     }
+
     return Promise.reject(err);
   }
 );
