fbffe6d7fe13bb2e650089b4fefb2827ba352eca
Harshusinghal2802 <harshsinghal2820@gmail.com>
2026-07-03 13:50:56 +0530

    Initial commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,10 @@
-import axios from 'axios';
-
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
 // Request interceptor - attach token
 api.interceptors.request.use(
   (config) => {
